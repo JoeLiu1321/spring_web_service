0b0213d7f5a86905fe619765338e955edd205c0a
JoeLiu <angrybirdliu@gmail.com>
2020-09-06 18:31:19 +0800

    add dockerfile modify docker-compose file

--- CakeShopService/src/main/java/application/Application.java
+++ CakeShopService/src/main/java/application/Application.java
@@ -10,14 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
     @Bean
     public CommandLineRunner setUp(RoleRepository roleRepository, RepositoryFacade facade) {
         return (args -> {
@@ -28,18 +27,17 @@ public class Application {
         });
     }
 
-    private void createDefaultRoles(RoleRepository roleRepository){
-        for(RoleEnum roleEnum:RoleEnum.values()) {
-            if(!roleRepository.findById(roleEnum.ordinal()).isPresent())
-                roleRepository.save(new Role(roleEnum.ordinal(),roleEnum.name()));
+    private void createDefaultRoles(RoleRepository roleRepository) {
+        for (RoleEnum roleEnum : RoleEnum.values()) {
+            if (!roleRepository.findById(roleEnum.ordinal()).isPresent())
+                roleRepository.save(new Role(roleEnum.ordinal(), roleEnum.name()));
         }
     }
 
-    private void createDefaultAccount(RepositoryFacade facade){
-        String accountName="admin",password="password",
-                name="Boss",phone="0987654321",address="Taipei"
-                ,email="admin@gmail.com";
-        if(!facade.findAccount(accountName).isPresent()) {
+    private void createDefaultAccount(RepositoryFacade facade) {
+        String accountName = "admin", password = "password", name = "Boss", phone = "0987654321", address = "Taipei",
+                email = "admin@gmail.com";
+        if (!facade.findAccount(accountName).isPresent()) {
             AccountInfo accountInfo = new AccountInfo(name, phone, address, email);
             accountInfo.setRole(new Role(RoleEnum.ADMIN));
             Account account = new Account(accountName, password);
@@ -48,12 +46,12 @@ public class Application {
         }
     }
 
-    private void createDefaultProduct(RepositoryFacade facade){
-        String[]productNames=new String[]{"草莓蛋糕","輕乳酪蛋糕","測試蛋糕"};
-        Integer productPrice=200;
-        if(!facade.findProduct().iterator().hasNext()){
-            for(String productName:productNames){
-                Product product=new Product(productName,"這是"+productName,productPrice);
+    private void createDefaultProduct(RepositoryFacade facade) {
+        String[] productNames = new String[] { "草莓蛋糕", "輕乳酪蛋糕", "測試蛋糕" };
+        Integer productPrice = 200;
+        if (!facade.findProduct().iterator().hasNext()) {
+            for (String productName : productNames) {
+                Product product = new Product(productName, "這是" + productName, productPrice);
                 product.setOnSell(true);
                 product.setVolume(500);
                 facade.createProduct(product);
--- CakeShopService/src/main/java/application/controllers/AccountController.java
+++ CakeShopService/src/main/java/application/controllers/AccountController.java
@@ -8,7 +8,6 @@ import application.adapter.output.MessageOutputAdapter;
 import application.entities.Account;
 import application.entities.AccountInfo;
 import application.repositories.AccountInfoRepository;
-import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -21,7 +20,7 @@ import java.util.Optional;
 
 @Controller
 @CrossOrigin
-@RequestMapping(path="/account")
+@RequestMapping(path = "/account")
 public class AccountController {
     @Autowired
     private JavaMailSender mailSender;
@@ -31,79 +30,76 @@ public class AccountController {
     private AccountInfoRepository accountInfoRepository;
     @Autowired
     private ResponseFactory responseFactory;
-    @RequestMapping(method = RequestMethod.GET, path="/test")
-    public @ResponseBody String testRequest(){
+
+    @RequestMapping(method = RequestMethod.GET, path = "/test")
+    public @ResponseBody String testRequest() {
         return "success";
     }
+
     @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody
-    MessageOutputAdapter createAccount(@RequestBody Account account){
-        Optional<String>accountName=Optional.ofNullable(account.getAccountName());
-        if(!accountName.isPresent())
+    public @ResponseBody MessageOutputAdapter createAccount(@RequestBody Account account) {
+        Optional<String> accountName = Optional.ofNullable(account.getAccountName());
+        if (!accountName.isPresent())
             return responseFactory.fieldNotPresent("accountName");
-        else{
+        else {
             boolean isUsed = facade.findAccount(accountName.get()).isPresent();
             if (isUsed)
                 return responseFactory.accountHasBeenUsed();
             else {
-                Integer accountId= facade.createAccount(account);
+                Integer accountId = facade.createAccount(account);
                 return responseFactory.outputData(accountId);
             }
         }
     }
 
-    @RequestMapping(path="/{accountName}", method = RequestMethod.DELETE)
-    public @ResponseBody
-    MessageOutputAdapter deleteAccount(@RequestParam String sessionKey, @RequestParam String sessionValue, @PathVariable String accountName){
-        if(facade.isSessionExist(sessionKey,sessionValue)){
-            Optional<Account> account= facade.findAccount(accountName);
-            if(!account.isPresent())
+    @RequestMapping(path = "/{accountName}", method = RequestMethod.DELETE)
+    public @ResponseBody MessageOutputAdapter deleteAccount(@RequestParam String sessionKey,
+            @RequestParam String sessionValue, @PathVariable String accountName) {
+        if (facade.isSessionExist(sessionKey, sessionValue)) {
+            Optional<Account> account = facade.findAccount(accountName);
+            if (!account.isPresent())
                 return responseFactory.dataNotFound("account");
             else {
                 facade.deleteAccount(account.get().getAccountName());
                 return responseFactory.success();
             }
-        }
-        else{
+        } else {
             return responseFactory.sessionTimeout();
         }
     }
 
-    @RequestMapping(path="/login", method = RequestMethod.POST)
-    public @ResponseBody
-    MessageOutputAdapter login(@RequestParam String accountName, @RequestParam String password){
-        if(facade.login(accountName,password)) {
-            Session session= facade.createSession(accountName);
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public @ResponseBody MessageOutputAdapter login(@RequestParam String accountName, @RequestParam String password) {
+        if (facade.login(accountName, password)) {
+            Session session = facade.createSession(accountName);
             return responseFactory.outputData(session);
-        }
-        else
+        } else
             return responseFactory.accountOrPasswordError();
     }
 
-    @RequestMapping(path="/logout/{accountName}", method = RequestMethod.POST)
-    public @ResponseBody
-    MessageOutputAdapter logout(@PathVariable String accountName){
-        Optional<Account> account= facade.findAccount(accountName);
-        if(account.isPresent()) {
+    @RequestMapping(path = "/logout/{accountName}", method = RequestMethod.POST)
+    public @ResponseBody MessageOutputAdapter logout(@PathVariable String accountName) {
+        Optional<Account> account = facade.findAccount(accountName);
+        if (account.isPresent()) {
             facade.deleteSession(accountName);
             return responseFactory.success();
-        }
-        else
+        } else
             return responseFactory.accountOrPasswordError();
     }
 
-    @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody
-    MessageOutputAdapter getAll(@RequestParam String sessionKey, @RequestParam String sessionValue){
-        if(!facade.isSessionExist(sessionKey,sessionValue)) {
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody MessageOutputAdapter getAll(@RequestParam String sessionKey,
+            @RequestParam String sessionValue) {
+        if (!facade.isSessionExist(sessionKey, sessionValue)) {
             return responseFactory.sessionTimeout();
         }
         return responseFactory.outputData(outputAccounts(facade.findAccount()));
     }
 
-    @RequestMapping(path="/{accountName}", method=RequestMethod.GET,params = {"sessionKey","sessionValue"})
-    public @ResponseBody MessageOutputAdapter getAccount(@RequestParam String sessionKey, @RequestParam String sessionValue, @PathVariable String accountName) {
-        if(!facade.isSessionExist(sessionKey,sessionValue))
+    @RequestMapping(path = "/{accountName}", method = RequestMethod.GET, params = { "sessionKey", "sessionValue" })
+    public @ResponseBody MessageOutputAdapter getAccount(@RequestParam String sessionKey,
+            @RequestParam String sessionValue, @PathVariable String accountName) {
+        if (!facade.isSessionExist(sessionKey, sessionValue))
             return responseFactory.sessionTimeout();
         Optional<Account> account = facade.findAccount(accountName);
         if (account.isPresent())
@@ -112,29 +108,28 @@ public class AccountController {
             return responseFactory.dataNotFound("account");
     }
 
-    @RequestMapping(path="/{id}",method = RequestMethod.GET)
-    public @ResponseBody MessageOutputAdapter isAccountExisted(@PathVariable Integer id){
-        Optional<Account> account=facade.findAccount(id);
-        if(account.isPresent()) {
-            String name=account.get().getInfo().getName();
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody MessageOutputAdapter isAccountExisted(@PathVariable Integer id) {
+        Optional<Account> account = facade.findAccount(id);
+        if (account.isPresent()) {
+            String name = account.get().getInfo().getName();
             return responseFactory.outputData(name);
-        }
-        else
+        } else
             return responseFactory.dataNotFound("account");
     }
 
-    @PatchMapping(path="/{accountName}")
-    public @ResponseBody
-    MessageOutputAdapter update(@RequestParam String sessionKey, @RequestParam String sessionValue, @PathVariable String accountName, @RequestBody Account account) {
+    @PatchMapping(path = "/{accountName}")
+    public @ResponseBody MessageOutputAdapter update(@RequestParam String sessionKey, @RequestParam String sessionValue,
+            @PathVariable String accountName, @RequestBody Account account) {
         Optional<Account> existedAccount = facade.findAccount(accountName);
-        if(!facade.isSessionExist(sessionKey,sessionValue))
+        if (!facade.isSessionExist(sessionKey, sessionValue))
             return responseFactory.sessionTimeout();
-        else{
-            if(!existedAccount.isPresent())
+        else {
+            if (!existedAccount.isPresent())
                 return createAccount(account);
-            else{
+            else {
                 Account updatedAccount = existedAccount.get();
-                AccountInfo oldInfo=updatedAccount.getInfo();
+                AccountInfo oldInfo = updatedAccount.getInfo();
                 updatedAccount.setAccount(account);
                 facade.createAccount(updatedAccount);
                 accountInfoRepository.delete(oldInfo);
@@ -143,14 +138,14 @@ public class AccountController {
         }
     }
 
-    @RequestMapping(path = "/mail/{mail}",method = RequestMethod.GET)
-    public @ResponseBody MessageOutputAdapter validateEmail(@PathVariable String mail){
-        Optional<Account> account=facade.findAccount(mail);
-        if(account.isPresent())
+    @RequestMapping(path = "/mail/{mail}", method = RequestMethod.GET)
+    public @ResponseBody MessageOutputAdapter validateEmail(@PathVariable String mail) {
+        Optional<Account> account = facade.findAccount(mail);
+        if (account.isPresent())
             return responseFactory.accountHasBeenUsed();
         else {
             Session session = facade.createSession(mail);
-            String content="你的驗證碼是 : "+session.getValue()+"\n請於15分鐘內至網頁輸入,完成您的信箱驗證";
+            String content = "你的驗證碼是 : " + session.getValue() + "\n請於15分鐘內至網頁輸入,完成您的信箱驗證";
             SimpleMailMessage msg = new SimpleMailMessage();
             msg.setTo(mail);
             msg.setSubject("味難忘甜品屋信箱驗證");
@@ -160,22 +155,23 @@ public class AccountController {
         }
     }
 
-    @RequestMapping(path="/session",method = RequestMethod.GET, params = {"sessionKey","sessionValue"})
-    public @ResponseBody MessageOutputAdapter checkSession(@RequestParam String sessionKey, @RequestParam String sessionValue){
-        if(facade.isSessionExist(sessionKey,sessionValue))
+    @RequestMapping(path = "/session", method = RequestMethod.GET, params = { "sessionKey", "sessionValue" })
+    public @ResponseBody MessageOutputAdapter checkSession(@RequestParam String sessionKey,
+            @RequestParam String sessionValue) {
+        if (facade.isSessionExist(sessionKey, sessionValue))
             return responseFactory.success();
         else
             return responseFactory.sessionTimeout();
     }
 
-    public Iterable<AccountAdapter> outputAccounts(Iterable<Account>accounts){
-        List<AccountAdapter> returnAccounts=new ArrayList<>();
-        for(Account account:accounts)
+    public Iterable<AccountAdapter> outputAccounts(Iterable<Account> accounts) {
+        List<AccountAdapter> returnAccounts = new ArrayList<>();
+        for (Account account : accounts)
             returnAccounts.add(outputAccount(account));
         return returnAccounts;
     }
 
-    public AccountAdapter outputAccount(Account account){
+    public AccountAdapter outputAccount(Account account) {
         return new AccountAdapter(account);
     }
 
